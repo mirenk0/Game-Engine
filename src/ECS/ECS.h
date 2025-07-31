b7c8c83c893bf86622e56b3e566afc17fbbeb9b5
mirenk0 <mirenkodaci21@gmail.com>
2025-07-31 20:47:20 +0200

    Add Pool and start Registry

--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -64,12 +64,61 @@ public:
   std::vector<Entity> GetSystemEntities() const;
   const Signature &GetComponentSignature() const;
 
-  // Defines the component type that entities must have to be considered by
-  // the system
+  // Defines the component type that entities must have to be considered by the
+  // system
   template <typename TComponent> void RequireComponent();
 };
 
-class Registry {};
+////////////////////////////////////////////////////////////////////////////////
+// Pool
+////////////////////////////////////////////////////////////////////////////////
+// A pool is just a vector (contiguous data) of objects of type T
+////////////////////////////////////////////////////////////////////////////////
+class IPool {
+public:
+  virtual ~IPool() {}
+};
+
+template <typename T> class Pool : public IPool {
+private:
+  std::vector<T> data;
+
+public:
+  Pool(int size = 100) { data.resize(size); }
+
+  virtual ~Pool() = default;
+
+  bool isEmpty() const { return data.empty(); }
+
+  int GetSize() const { return data.size(); }
+
+  void Resize(int n) { data.resize(n); }
+
+  void Clear() { data.clear(); }
+
+  void Add(T object) { data.push_back(object); }
+
+  void Set(int index, T object) { data[index] = object; }
+
+  T &Get(int index) { return static_cast<T &>(data[index]); }
+
+  T &operator[](unsigned int index) { return data[index]; }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Registry
+////////////////////////////////////////////////////////////////////////////////
+// The registry manages the creation and destruction of entities, add systems,
+// and components.
+////////////////////////////////////////////////////////////////////////////////
+class Registry {
+private:
+  int numEntities = 0;
+
+  // Vector of component pools, each pool contains all the data for a certain
+  // compoenent type [Vector index = component type id] [Pool index = entity id]
+  std::vector<IPool *> componentPools;
+};
 
 template <typename TComponent> void System::RequireComponent() {
   const auto componentId = Component<TComponent>::GetId();
